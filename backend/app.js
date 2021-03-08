159b58e89ef10db392f24249f62ee9ef76322a46
aviaryan77 <aviaryan77@gmail.com>
2021-03-08 22:51:23 +0530

    started with server

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+require("dotenv/config");
+const api = process.env.API_URL;
+
+// http://localhost:3000/api/v1/products
+// also used as
+// app.get(api+"/products", (req, res) => {
+
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "hairdresser",
+    image: "some_URL",
+  };
+  res.send("Hello API");
+});
+app.listen(3000, () => {
+  console.log("server is  running on http 3000");
+  console.log(api);
+});

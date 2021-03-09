133a3384db1d9b46569c1f08d282e6f4fd9d35ea
aviaryan77 <aviaryan77@gmail.com>
2021-03-09 19:23:15 +0530

    user module with jwt token

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,14 +1,14 @@
-const {Order} = require('../models/order');
-const express = require('express');
+const { Order } = require("../models/order");
+const express = require("express");
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
-    const orderList = await Order.find();
+router.get(`/`, async (req, res) => {
+  const orderList = await Order.find();
 
-    if(!orderList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(orderList);
-})
+  if (!orderList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(orderList);
+});
 
-module.exports =router;
+module.exports = router;
